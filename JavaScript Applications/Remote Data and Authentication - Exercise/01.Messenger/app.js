bdef15b06285af2521cdc6d286d7162cdfc6b9da
theospasov <97851719+theospasov@users.noreply.github.com>
2023-03-14 23:47:15 +0200

    problem 1

---
+++ JavaScript Applications/Remote Data and Authentication - Exercise/01.Messenger/app.js
@@ -0,0 +1,10 @@
+function attachEvents() {
+    const baseURL = `http://localhost:3030/jsonstore/messenger`
+
+    const allMessagesSection = document.getElementById('messages')
+    const submitBtn = document.getElementById('submit')
+    const refreshBtn = document.getElementById('refresh')
+    const nameArea = document.getElementById('author')
+}
+
+attachEvents();
