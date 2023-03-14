41fc4f3c0666d4fb1b7df643e12196852eb86585
theospasov <97851719+theospasov@users.noreply.github.com>
2023-03-15 00:00:45 +0200

    problem 1

--- JavaScript Applications/Remote Data and Authentication - Exercise/01.Messenger/app.js
+++ JavaScript Applications/Remote Data and Authentication - Exercise/01.Messenger/app.js
@@ -4,7 +4,34 @@ function attachEvents() {
     const allMessagesSection = document.getElementById('messages')
     const submitBtn = document.getElementById('submit')
     const refreshBtn = document.getElementById('refresh')
-    const nameArea = document.getElementById('author')
+    const nameArea = document.querySelector('input[name="author"]')
+    const messageArea = document.querySelector('input[name="content"]')
+
+    submitBtn.addEventListener('click', () => {
+
+        let messageObj = {
+            author: nameArea.value,
+            content: messageArea.value
+
+        }
+
+        fetch(baseURL, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(messageObj)
+        })
+    })
+
+    refreshBtn.addEventListener('click', () => {
+        fetch(baseURL)
+        .then(res => res.json())
+        .then(res => {
+            allMessagesSection.textContent = res
+        })
+    })
+
 }
 
 attachEvents();
