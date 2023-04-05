93f3eadc3a67727411d14a185aa4e24ce493b8b0
theospasov <97851719+theospasov@users.noreply.github.com>
2023-04-05 22:50:55 +0300

    exam prep 2

---
+++ 3. JavaScript Advanced - Jan 2023/Exam Prep/2023.01/01. Mails Delivery/app.js
@@ -0,0 +1,124 @@
+// window.addEventListener('load', solve);
+
+
+function solve() {
+
+    // 0.Capture all Elements
+
+        // 0.1 Capture Add Mails Div
+            //0.1.1 Add Mails Section
+            let recipientInput1 = document.getElementById('recipientName')
+            let titleInput1 = document.getElementById('title')
+            let messageInput1 = document.getElementById('message')
+
+            let btnAddToTheList = document.getElementById('add')
+            let btnReset = document.getElementById('reset')
+
+            // let generalInputRecipirnt = ''
+            // let generalInputTitle = ''
+            // let generaInputMessage = ''
+
+            //0.1.2 List of Mails Section
+            let sectionListOfMails = document.getElementById('list')
+
+            let sectionSentMail = document.getElementById('sent-list')
+            
+
+    // 1. All fields are non-empty strings
+
+
+    
+    
+    //console.log(`the fields aren't empty`);
+
+        //1.1 Reset btn
+        btnReset.addEventListener('click', (e)=> {
+            e.preventDefault()
+            if(recipientInput1.value != '' && titleInput1.value != '' && messageInput1.value != '' ) {
+            recipientInput1.value = ''
+            titleInput1.value = ''
+            messageInput1.value = ''
+            }
+        })
+
+        //1.2 Add to list btn
+        btnAddToTheList.addEventListener('click', (e)=> {
+            e.preventDefault()
+
+            if(recipientInput1.value != '' && titleInput1.value != '' && messageInput1.value != '' ) {
+
+              
+            //1.2.1 Creating new elements
+            
+            newliEl1 = document.createElement('li')
+            sectionListOfMails.appendChild(newliEl1)
+            
+            newh4El1 = document.createElement('h4')
+            newh4El1.textContent = 'Title: ' + titleInput1.value
+            newliEl1.appendChild(newh4El1)
+
+            let newh4El2 = document.createElement('h4')
+            newh4El2.textContent = 'Recipient Name: ' + recipientInput1.value
+            newliEl1.appendChild(newh4El2)
+
+            let newspanEl = document.createElement('span')
+            newspanEl.textContent = messageInput1.value
+            newliEl1.appendChild(newspanEl)
+
+            let newdivEl = document.createElement('div')
+            newdivEl.setAttribute('id', 'list-action')
+            newliEl1.appendChild(newdivEl)
+
+            let newbtn1 = document.createElement('button')
+            newbtn1.setAttribute('type', 'submit')
+            newbtn1.setAttribute('id', 'send')
+            newbtn1.textContent = 'Send'
+            newdivEl.appendChild(newbtn1)
+
+            let newbtn2 = document.createElement('button')
+            newbtn2.setAttribute('type', 'submit')
+            newbtn2.setAttribute('id', 'delete')
+            newbtn2.textContent = 'Delete'
+            newdivEl.appendChild(newbtn2)
+
+            }
+
+            recipientInput1.value = ''
+            titleInput1.value = ''
+            messageInput1.value = ''
+
+
+            // SEND BUTTON 
+            let btnSend = document.getElementById('send')
+
+            btnSend.addEventListener('click', (e) =>  {
+               
+                let newEl8SentMail8Li = document.createElement('li')
+                let textUn = e.target.parentElement.parentElement
+                console.log(textUn)
+            
+
+                
+
+
+                
+            })
+
+        })
+
+    // 2. Send Mails
+    
+    
+
+
+        
+
+
+
+    // let toDeleteTheOldInformation = sectionListOfMails.children[0]
+    // toDeleteTheOldInformation.remove()
+
+
+
+}
+solve()
---
+++ 3. JavaScript Advanced - Jan 2023/Exam Prep/2023.01/02. Car Dealership/carDealership.js
@@ -0,0 +1,143 @@
+class CarDealership {
+    constructor(name) {
+        this.name = name
+        this.availableCars = []
+        this.soldCars = []
+        this.totalIncome = 0
+    }
+
+    addCar(model, horsepower, price, mileage) {
+        if (model == '' || horsepower < 0 || !Number.isInteger(horsepower) || price < 0 || mileage < 0) {
+            throw new Error ("Invalid input!")
+        }
+
+
+        this.availableCars.push({model, horsepower, price, mileage })
+        return `New car added: ${model} - ${horsepower} HP - ${mileage.toFixed(2)} km - ${price.toFixed(2)}$`
+
+
+
+
+    }
+
+    sellCar(model, desiredMileage) {
+        let carIsAvailable = false
+        let currCar = null
+
+        for (const currEl of this.availableCars) {
+            if(currEl.model == model ) {
+                carIsAvailable = true
+                currCar = currEl
+            }
+        }
+
+        if(!carIsAvailable) {
+            throw new Error (`${model} was not found!`) 
+        } 
+
+        let difference = currCar.mileage - desiredMileage
+
+        if(currCar.mileage <= desiredMileage) {
+            currCar.price =  currCar.price.toFixed(2)
+        } else if (difference <= 40000) {
+            currCar.price = (currCar.price * 0.95).toFixed(2)
+        } else if (difference > 40000) {
+            currCar.price = (currCar.price * 0.90).toFixed(2)
+        }
+
+        let soldCar = {
+            model:  currCar.model,
+            horsepower: currCar.horsepower,
+            price:  currCar.price
+        }
+        this.totalIncome += Number(currCar.price)
+        this.soldCars.push(soldCar)
+        
+        // console.table( this.availableCars);
+        // console.table(this.soldCars)
+        this.availableCars.filter(x => x != currCar)
+        return `${currCar.model} was sold for ${currCar.price}$`
+
+    }
+
+    currentCar() {
+
+        let finalText = '-Available cars:'
+        
+        if(this.availableCars.length == 0) {
+            return `There are no available cars`
+        } else {
+           // console.log(this.availableCars)
+            for (const currEl of this.availableCars) {
+                finalText += `\n---${currEl.model} - ${currEl.horsepower} HP - ${(currEl.mileage).toFixed(2)} km - ${(currEl.price).toFixed(2)}$`
+            }
+        }
+        return finalText
+    }
+    salesReport(criteria) {
+        let sortedArray = null
+        let finalText = `-${this.name} has a total income of ${this.totalIncome.toFixed(2)}$\n-${this.soldCars.length} cars sold:`
+
+        let correctCriteria = false 
+
+        if (criteria == 'horsepower') {
+            correctCriteria = true
+        }
+
+        if (criteria == 'model') {
+            correctCriteria = true
+        }
+        
+
+        if(!correctCriteria ) {
+            throw new Error ("Invalid criteria!")
+        }
+
+        if (criteria == 'horsepower' ) {
+            sortedArray = this.soldCars.sort((a,b) => b.horsepower - a.horsepower)
+            for (const iterator of sortedArray) {
+                
+                finalText += `\n---${iterator.model} - ${iterator.horsepower} HP - ${iterator.price}$`
+            }
+        }
+        
+        if (criteria == 'model' ) {
+            sortedArray = this.soldCars.sort((a,b) => (a.model).localeCompare(b.model))
+            for (const iterator of sortedArray) {
+                
+                finalText += `\n---${iterator.model} - ${iterator.horsepower} HP - ${iterator.price}$`
+            }
+        }
+
+        return finalText
+
+        
+    }
+
+}
+
+// let dealership = new CarDealership('SoftAuto');
+// console.log(dealership.addCar('Toyota Corolla', 100, 3500, 190000));
+// console.log(dealership.addCar('Mercedes C63', 300, 29000, 187000));
+// console.log(dealership.addCar('', 120, 4900, 240000));
+
+// let dealership = new CarDealership('SoftAuto');
+// dealership.addCar('Toyota Corolla', 100, 3500, 190000);
+// dealership.addCar('Mercedes C63', 300, 29000, 187000);
+// dealership.addCar('Audi A3', 120, 4900, 240000);
+// console.log(dealership.sellCar('Toyota Coraalla', 230000));
+// console.log(dealership.sellCar('Mercedes C63', 110000));
+
+// let dealership = new CarDealership('SoftAuto');
+// dealership.addCar('Toyota Corolla', 100, 3500, 190000);
+// dealership.addCar('Mercedes C63', 300, 29000, 187000);
+// dealership.addCar('Audi A3', 120, 4900, 240000);
+// console.log(dealership.currentCar());
+
+let dealership = new CarDealership('SoftAuto');
+dealership.addCar('Toyota Corolla', 100, 3500, 190000);
+dealership.addCar('Mercedes C63', 300, 29000, 187000);
+dealership.addCar('Audi A3', 120, 4900, 240000);
+dealership.sellCar('Toyota Corolla', 230000);
+dealership.sellCar('Mercedes C63', 110000);
+console.log(dealership.salesReport('horsepower'));
