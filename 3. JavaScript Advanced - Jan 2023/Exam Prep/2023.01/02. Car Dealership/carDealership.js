1b8bb53f35905848bb4b1b48240bda2878bc02f2
theospasov <97851719+theospasov@users.noreply.github.com>
2023-04-09 22:48:13 +0300

    update

--- 3. JavaScript Advanced - Jan 2023/Exam Prep/2023.01/02. Car Dealership/carDealership.js
+++ 3. JavaScript Advanced - Jan 2023/Exam Prep/2023.01/02. Car Dealership/carDealership.js
@@ -123,11 +123,6 @@ class CarDealership {
 // console.log(dealership.sellCar('Toyota Coraalla', 230000));
 // console.log(dealership.sellCar('Mercedes C63', 110000));
 
-// let dealership = new CarDealership('SoftAuto');
-// dealership.addCar('Toyota Corolla', 100, 3500, 190000);
-// dealership.addCar('Mercedes C63', 300, 29000, 187000);
-// dealership.addCar('Audi A3', 120, 4900, 240000);
-// console.log(dealership.currentCar());
 
 let dealership = new CarDealership('SoftAuto');
 dealership.addCar('Toyota Corolla', 100, 3500, 190000);
