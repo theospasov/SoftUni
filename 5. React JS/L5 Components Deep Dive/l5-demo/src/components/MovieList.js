f2e48e6a6a6cbca8c182e8a04bae4c87147cc919
theospasov <97851719+theospasov@users.noreply.github.com>
2023-05-01 23:15:40 +0300

    l5 part 1 classwork

---
+++ 5. React JS/L5 Components Deep Dive/l5-demo/src/components/MovieList.js
@@ -0,0 +1,27 @@
+import Movie from "./Movie";
+
+
+export default function MovieList({movies}) { {/* 1 */}
+
+    return (
+        <ul>
+            {movies.map((movie) => (
+                <li key={movie.id} >  {/*2 */}
+                    <Movie {...movie}/> {/* 3  */}
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+{/* 1. Here we're using Object destructuring. */}
+
+{/* 2. Each React Element from a List Component should have an unique key */}
+
+{/* 3. We're using the spread operator to make Props with the keys of the Object. Without it, it will have to look like:
+<Movie
+    title={movie.title}
+    year={movie.year}
+    plot={movie.plot}
+    posterUrl={movie.posterUrl} 
+/>  */}
