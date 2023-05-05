43926f1f97e679c6939ae82001dfea42a9f4d2c6
theospasov <97851719+theospasov@users.noreply.github.com>
2023-05-05 23:59:30 +0300

    project 1

---
+++ 5. React JS/Scrimba/1.0 Learn React/1. React Site/Fun facts about React - project/vite-local-one/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import { Main } from './components/Main'
+import { Navbar } from './components/Navbar'
+
+function App() {
+
+
+  return (
+    <div className='site'>
+      <Navbar/>
+      <Main/>
+    </div>
+  )
+}
+
+export default App
+
+{/*import './App.css' */}
---
+++ 5. React JS/Scrimba/1.0 Learn React/1. React Site/Fun facts about React - project/vite-local-one/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export function Navbar(){
+    return(
+        <nav className="navigation">
+            <div className="logo">
+                <img src="../../public/R.png" width="60px"/>
+                <h3>ReactFacts</h3>
+            </div>
+            <p>React Course - Project 1</p>
+        </nav>
+    )
+}
