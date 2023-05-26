4665b5f65faf4e19965fced03e44ff25518226b0
theospasov <97851719+theospasov@users.noreply.github.com>
2023-05-26 18:27:49 +0300

    node l4 demo updated

--- 6. Back-End/L4 Node.js Streams and Utilities/demo/boardController.js
+++ 6. Back-End/L4 Node.js Streams and Utilities/demo/boardController.js
@@ -1,11 +1,25 @@
+const fs = require('fs')
+
 function homePage(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    })
+    fs.createReadStream('./static/index.html').pipe(res)
     res.write('home page')
     res.end()
 }
 
 function sendFile(req, res) {
-    res.write('static file')
-    res.end()
+    if (req.url == '/style.css') {
+        res.writeHead(200, {
+            'Content-Type': 'text/css'
+        })
+        fs.createReadStream('./static/style.css').pipe(res)
+    } else {
+       const filename = '.' + req.url
+       fs.createReadStream(filename).pipe(res)
+    }
+    
 }
 
 module.exports = {
