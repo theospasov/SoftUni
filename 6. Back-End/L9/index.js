9b9823292ca4ea711e498158a53a3bf7a4bd403a
theospasov <97851719+theospasov@users.noreply.github.com>
2023-06-10 12:14:35 +0300

    demo l9

---
+++ 6. Back-End/L9/index.js
@@ -0,0 +1,31 @@
+const server = require('http').createServer((req, res) => {
+    if (req.url == '/') { //
+        let visitedCounter = 0
+        if (req.headers.cookie) { 
+            const cookies = req.headers.cookie
+            .split(';') 
+            .map(c => c.trim()) 
+            .map(c => c.split('=')) 
+
+            const cookiesObject = Object.fromEntries(cookies)
+
+            if (cookiesObject.visited) {
+                visitedCounter = Number(cookiesObject.visited)
+                visitedCounter++ 
+            }
+
+            console.log(cookiesObject);
+        }
+        //console.log(req.headers.cookie);
+        res.writeHead(200, {
+            'Set-Cookie': `visited=${visitedCounter}; httpOnly`
+        })
+        res.write(`<p>Hello, you have visited this page ${visitedCounter}</p>`)
+        res.end()
+
+    } else {
+        res.writeHead(404)
+        res.end()
+    }
+
+}).listen(3000)
