35bdfd93e7c2602063d095f9dad52bf5e3988de2
theospasov <97851719+theospasov@users.noreply.github.com>
2023-07-07 23:53:41 +0300

    React - notes app updates

--- 5.1 React JS - Scrimba/1.0 Learn React/4. Build a notes app and Tenzies game/2. Notes app/App.jsx
+++ 5.1 React JS - Scrimba/1.0 Learn React/4. Build a notes app and Tenzies game/2. Notes app/App.jsx
@@ -14,6 +14,20 @@ import { notesCollection, db } from "./firebase"
 export default function App() {
     const [notes, setNotes] = React.useState([])
     const [currentNoteId, setCurrentNoteId] = React.useState("")
+    /**
+     * Challenge:
+     * 1. Set up a new state variable called `tempNoteText`. Initialize 
+     *    it as an empty string
+     * 2. Change the Editor so that it uses `tempNoteText` and 
+     *    `setTempNoteText` for displaying and changing the text instead
+     *    of dealing directly with the `currentNote` data.
+     * 3. Create a useEffect that, if there's a `currentNote`, sets
+     *    the `tempNoteText` to `currentNote.body`. (This copies the
+     *    current note's text into the `tempNoteText` field so whenever 
+     *    the user changes the currentNote, the editor can display the 
+     *    correct text.
+     * 4. TBA
+     */
     
     const currentNote =
         notes.find(note => note.id === currentNoteId)
--- 5.1 React JS - Scrimba/1.0 Learn React/4. Build a notes app and Tenzies game/2. Notes app/README.md
+++ 5.1 React JS - Scrimba/1.0 Learn React/4. Build a notes app and Tenzies game/2. Notes app/README.md
@@ -1,4 +1,4 @@
-# co8b6453fbbc5bbed0147c5cf
+# co1cc45f68cc600de2cd28cda
 
 Warning: Vite enforces using jsx syntax inside jsx/tsx files, so it will complain about that. Solution: rename `.js` files to `.jsx` :)
 
