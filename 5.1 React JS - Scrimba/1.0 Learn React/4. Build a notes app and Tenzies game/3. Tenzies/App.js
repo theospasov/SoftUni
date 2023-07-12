0fbd55d071258cbae44b53b5c3627e8a5b598677
theospasov <97851719+theospasov@users.noreply.github.com>
2023-07-12 23:14:06 +0300

    React - Tenzies update

--- 5.1 React JS - Scrimba/1.0 Learn React/4. Build a notes app and Tenzies game/3. Tenzies/App.js
+++ 5.1 React JS - Scrimba/1.0 Learn React/4. Build a notes app and Tenzies game/3. Tenzies/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Die from "./Die"
 
 /**
  * Challenge:
@@ -9,10 +10,28 @@ import React from "react"
  *        for now
  * - Style the <main> and <Die> components 
  *   to look like they do in the slide
+ *      - Hints: Create a container to hold the 10 instances
+ *        of the Die component, and use CSS Grid to lay them
+ *        out evenly in 2 rows of 5 columns
+ *      - Use flexbox on main to center the dice container
+ *        in the center of the page
  */
 
 export default function App() {
     return (
-        <main></main>
+        <main>
+            <div className="dice-container">
+                <Die value="1" />
+                <Die value="2" />
+                <Die value="3" />
+                <Die value="4" />
+                <Die value="5" />
+                <Die value="6" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+                <Die value="1" />
+            </div>
+        </main>
     )
 }
