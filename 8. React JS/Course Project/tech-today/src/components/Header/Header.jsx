c2eec541f66183574661a132a3cd6c3943ec5965
theospasov <97851719+theospasov@users.noreply.github.com>
2023-10-24 12:41:59 +0300

    React JS course project

---
+++ 8. React JS/Course Project/tech-today/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css'
+
+export const Header = () => {
+    return (
+        <>  
+            <header className='site-header'>
+                <button className='header-button'>Hello</button>
+                <p>This is the header part</p>
+            </header>
+
+        </>
+    )
+}
